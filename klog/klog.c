b14c4c059d48d594dc142cc88a508fa1f15e46ab
liudan <liudan@localhost.localdomain>
2018-08-13 19:20:55 +0800

    增加一些注释

--- klog/klog.c
+++ klog/klog.c
@@ -2,8 +2,8 @@
  *
  * 本程序在/proc里生成一个日志文件，为一些内核模块提供另一种日志打印机制。
  * 类似于printk与dmesg ，但是为了不影响系统日志，所以在另一个文件里输出一些特别的日志。
- *
- *
+ *  klog的ctx相应于一个子类，klog模块自己应该知道目前有多少个ctx并使用proc输出。方便用户了解都有谁使用了klog. -- 2018/8/13
+ * author: yaxinsn.
  *
  * */
 
@@ -54,6 +54,7 @@
 
 #include "klog.h"
 
+#define KDEBUG printk
 
 
 
@@ -73,12 +74,10 @@ static int  klog_read (struct seq_file *m, void *v)
     #endif
         end = t->log_head-1;
         
-        printk("klog_read the around is 1 \n");
-        //seq_nprintf(m,(t->logbuf_size - (t->log_head-1 - t->log_buf)),"%s",start);
+        KDEBUG("klog_read the around is 1 \n");
         seq_printf(m,"%s",start);
         p = *(t->log_head);
         *t->log_head = 0;
-       // seq_nprintf(m,t->log_head - t->log_buf ,"%s",t->log_buf);
         seq_printf(m,"%s",t->log_buf);
         *t->log_head = p;
     }
@@ -87,12 +86,11 @@ static int  klog_read (struct seq_file *m, void *v)
         start = t->log_buf;
         end = t->log_head;
         
-        //seq_nprintf(m,t->log_head - t->log_buf,"%s",start);
         seq_printf(m,"%s",start);
     }
     
-    printk("ctx->log_buf %p  head %p  start %p \n",t->log_buf, t->log_head,start);
-    printk("%s", t->log_buf);
+    KDEBUG("ctx->log_buf %p  head %p  start %p \n",t->log_buf, t->log_head,start);
+    KDEBUG("%s", t->log_buf);
 
     
     return 0;
@@ -108,7 +106,8 @@ static ssize_t klog_write(struct file *file, const char __user *buffer, size_t c
 
     struct klog_ctx*  t =(struct klog_ctx* )((struct seq_file*)file->private_data)->private;
 											
-
+	//TODO;
+	// user write any ,and clear the klog_buf;
 	//char buf[20];
    	//int a;
    	//copy_from_user(&buf,buffer,20);
@@ -129,22 +128,23 @@ static const struct file_operations klog_fops = {
 		.write          = klog_write,
 };
 
-
+#define KLOG_BULK_SIZE 1024
 struct klog_ctx*  create_klog(char* file_name, struct proc_dir_entry* proc_dir,size_t size)
 {
     struct proc_dir_entry *proc_entry  = NULL;
+    char* log_buf;
 	struct klog_ctx* ctx = kmalloc(sizeof(struct klog_ctx),GFP_KERNEL|__GFP_ZERO);
 	if(!ctx)
 		return NULL;
 
 	
-	char* log_buf = kmalloc(20*size,GFP_KERNEL|__GFP_ZERO);	
+	log_buf = kmalloc(KLOG_BULK_SIZE*size,GFP_KERNEL|__GFP_ZERO);	
 	if(!log_buf){
 		printk("alloc pages is failed! \n");
 		goto err1;
 	}
     ctx->log_buf = log_buf;
-    ctx->logbuf_size = 20*size - 1;
+    ctx->logbuf_size = KLOG_BULK_SIZE*size - 1;
     
 	proc_entry = proc_create_data(file_name, 0, proc_dir, &klog_fops, ctx);
 	if(proc_entry == NULL)
@@ -190,7 +190,7 @@ EXPORT_SYMBOL(create_klog);
 
 int destroy_klog(struct klog_ctx* ctx )
 {
-    printk("----ctx->file_name %s -----\n",ctx->file_name);
+    KDEBUG("----ctx->file_name %s -----\n",ctx->file_name);
     remove_proc_entry(ctx->file_name,ctx->parent_entry);
     
     __KFREE(ctx->file_name);
@@ -206,9 +206,8 @@ void __log_store(struct klog_ctx* ctx,char* textbuf,int text_len)
 {
     //check around;
     u64 remain = (ctx->logbuf_size - (ctx->log_head - ctx->log_buf));
-    printk("size %d, (ctx->log_head - ctx->log_buf) %d \n",
-    ctx->logbuf_size, (ctx->log_head - ctx->log_buf));
-    printk("%s: text_len %d \n",__func__,text_len);
+    KDEBUG("size %d, (ctx->log_head - ctx->log_buf) %ld \n", ctx->logbuf_size,(ctx->log_head - ctx->log_buf));
+    KDEBUG("%s: text_len %d \n",__func__,text_len);
     if(text_len > remain)
     {
         memcpy(ctx->log_head,textbuf,remain);
@@ -230,7 +229,6 @@ asmlinkage int _vprintk_emit(struct klog_ctx* ctx,
 			    const char *fmt, va_list args)
 {
     int this_cpu;
-	int printed_len = 0;
 	static char textbuf[1024];
 	char *text = textbuf;
 	size_t text_len;
@@ -242,6 +240,7 @@ asmlinkage int _vprintk_emit(struct klog_ctx* ctx,
 	text_len = vscnprintf(text, sizeof(textbuf), fmt, args);
 	__log_store( ctx,textbuf,text_len);
 	spin_unlock(&ctx->lock);
+	return text_len;
 }
 int klog_printk(struct klog_ctx* ctx,const char *fmt, ...)
 {
@@ -250,6 +249,7 @@ int klog_printk(struct klog_ctx* ctx,const char *fmt, ...)
 	va_start(args, fmt);
 	r = _vprintk_emit(ctx,fmt, args);
 	va_end(args);
+	return r;
 }
 
 EXPORT_SYMBOL(klog_printk);
